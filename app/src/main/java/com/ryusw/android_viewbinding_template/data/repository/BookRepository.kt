e72038f63bd5bc4120627b3430992ce55c96fee2
DESKTOP-ORQQ32F\YeongJoo Yoo HP_Laptop <yyj9301@gmail.com>
2025-02-02 21:12:33 +0900

    hp 노트북 테스트 커밋

--- app/src/main/java/com/ryusw/android_viewbinding_template/data/repository/BookRepository.kt
+++ app/src/main/java/com/ryusw/android_viewbinding_template/data/repository/BookRepository.kt
@@ -3,6 +3,7 @@ package com.ryusw.android_viewbinding_template.data.repository
 import com.ryusw.android_viewbinding_template.data.remote.dto.BookDto
 import com.ryusw.android_viewbinding_template.data.remote.service.BookService
 
+
 // 로컬 및 통신을 통해 가져오는 데이터의 저장소  - 로컬이던, 리모트던 일단은 여기에 다 받는 곳
 // Sp는 단순 데이터이기때문에 object로 구현
 class BookRepository(
